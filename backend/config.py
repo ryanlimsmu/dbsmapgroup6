a9bce0bd08ec5543e0ee613ca0286d6a3c1f8312
dabroskii <paul.thang.0408@gmail.com>
2025-01-11 14:06:51 +0800

    configured jwt
    jwt works

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 import mysql.connector
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+from flask_jwt_extended import JWTManager
+
+>>>>>>> Stashed changes
 =======
 from flask_jwt_extended import JWTManager
 
